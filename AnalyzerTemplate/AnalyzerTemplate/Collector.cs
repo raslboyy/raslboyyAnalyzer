47e4bfa1a40c75cbadeae4f1491138990d366c13
ruslan <ruslankhakimov67@gmail.com>
2022-05-06 10:34:22 +0300

    ifAnalyzer

---
+++ AnalyzerTemplate/AnalyzerTemplate/Collector.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalyzerTemplate
+{
+    internal class Collector : CSharpSyntaxWalker
+    {
+        public ICollection<IfStatementSyntax> Collection { get; } = new List<IfStatementSyntax>();
+
+        public override void VisitIfStatement(IfStatementSyntax node)
+        {
+            Collection.Add(node);
+        }
+    }
+}
---
+++ AnalyzerTemplate/AnalyzerTemplate/ElseWalker.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnalyzerTemplate
+{
+    internal class ElseWalker : CSharpSyntaxWalker
+    {
+        public List<IfStatementSyntax> Collection = new List<IfStatementSyntax>();
+
+        public override void VisitIfStatement(IfStatementSyntax node)
+        {
+            if (node.Else != null)
+            {
+                
+                var innerCollector = new Collector();
+                innerCollector.Visit(node.Else);
+                foreach (var element in innerCollector.Collection)
+                {
+                    if (element.Parent.Kind() == SyntaxKind.ElseClause)
+                        continue;
+                    Collection.Add(element);
+                }
+            }
+            base.VisitIfStatement(node);
+        }
+    }
+}
